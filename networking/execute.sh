761a558dbe3e9570144d927fe5b37dc1c23cb66f
gkaoukis <geoka001@gmail.com>
2025-12-06 03:01:31 +0200

    added missing sudo

--- networking/execute.sh
+++ networking/execute.sh
@@ -135,7 +135,7 @@ if should_run "ssh-ips"; then
     export BENCHMARK_INPUT_FILE
     BENCHMARK_SCRIPT="$(realpath "$scripts_dir/ssh-ips.sh")"
     export BENCHMARK_SCRIPT
-    $KOALA_SHELL "$scripts_dir/ssh-ips.sh" root localhost open "$input_dir/ips_ssh_$size.txt"
+    sudo $KOALA_SHELL "$scripts_dir/ssh-ips.sh" root localhost open "$input_dir/ips_ssh_$size.txt"
     echo $?
 fi
 
